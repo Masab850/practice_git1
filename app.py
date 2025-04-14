a667fe74d56b1a0dc0215e8f76d71d8bbf0988a2
Masab850 <masabhassan4@gmail.com>
2025-04-14 14:33:14 +0500

    add new condation on the code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-a=10
-b=13
-if a>b:
+a = 10
+b = 13
+if a == b:
     print("you are right")
+elif a>b:
+    print("a is greater than b")
 else:
-    print("sorry you are wrong !")    
+    print("sorry you are wrong !")
